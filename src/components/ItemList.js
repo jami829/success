5a98e0ec2b6cfe361d26f08060a055c095c5d404
이재훈 <jami8291215@gmail.com>
2021-04-05 21:23:18 +0900

    :art: ItemList에서 Item으로 props 내리기

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,22 +5,33 @@ import axios from 'axios';
 import Item from '../view/Item';
 
 function ItemList() {
-  const [itemInfo, setItemInbfo] = useState([]);
+  const [itemInfo, setItemInfo] = useState([]);
 
   useEffect(() => {
     axios.get("https://rest.dealink.co.kr/auction/group/2?page=0&size=3&sort=createdDate,desc")
       .then((res) => {
-        console.log('res', res)
+        // console.log('res', res)
+        setItemInfo(res.data);
       })
       .catch((err) => {
-        console.log('err', err)
+        console.log('err', err.response.data)
       })
-  })
+  }, []);
+
+  console.log("itemInfo", itemInfo);
 
   return (
     <div>
       asdf
-      <Item />
+      {itemInfo.map((item, idx) => <Item
+        key={idx}
+        item={item}
+        imagePath={item.imagePath}
+        productName={item.productName}
+        currentPrice={item.currentPrice} // 경매 현재가로 각 카드에 가격 표시
+        count={item.count} // 찜한 사람 수, 좋아요 표시로 출력
+      />)}
+
     </div>
   )
 }
